0d54e0ed8e32f64dcec307a2fbcc48a278bff42e
alexsalm <154659521+alexsalm@users.noreply.github.com>
2025-09-01 13:03:57 -0700

    Add getComputerChoice() function that randomly returns either rock, paper, or scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    let choice = (Math.floor((Math.random() * 100) + 1));
+    if (choice < 33 && choice > 0) {
+        return 'rock';
+    } else if (choice < 67 && choice > 33) {
+        return 'paper';
+    } else if (choice < 100 && choice > 67) {
+        return 'scissors';
+    }
+}
+
+console.log(getComputerChoice());
